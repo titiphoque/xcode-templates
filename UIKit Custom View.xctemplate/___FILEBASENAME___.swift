b6af95551383c4a4e4a2aee2e40a1e9042255214
Thibaut Schmitt <t.schmitt@openium.fr>
2022-11-14 12:03:48 +0100

    Add UIKit Custom View template

---
+++ UIKit Custom View.xctemplate/___FILEBASENAME___.swift
@@ -0,0 +1,22 @@
+//
+// ___FILENAME___
+// ___PROJECTNAME___
+//
+// Created by ___FULLUSERNAME___ on ___DATE___.
+// ___COPYRIGHT___
+//
+
+import UIKit
+
+class ___FILEBASENAME___: UIView {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Custom setup
+    }
+    
+    // MARK: - Configure
+    
+    func configure(<#parameter#>: <#Type#>) {
+    }
+}
