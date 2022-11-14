289df3d81f77223edc42f2522fc6d73ddbccc730
Thibaut Schmitt <t.schmitt@openium.fr>
2022-11-14 12:03:24 +0100

    Add Protocol with Extension template Renaming CustomVuewClassUIKit to be more understandable

---
+++ Protocol With Extension.xctemplate/___FILEBASENAME___.swift
@@ -0,0 +1,27 @@
+//
+// ___FILENAME___
+// ___PROJECTNAME___
+//
+// Created by ___FULLUSERNAME___ on ___DATE___.
+// ___COPYRIGHT___
+//
+
+import Foundation
+
+protocol ___FILEBASENAME___ {
+    func <#functionName#>(<#parameter#>: <#Type#>)
+}
+
+// Default extension
+extension ___FILEBASENAME___ {
+    func <#functionName#>(<#parameter#>: <#Type#>) {
+        <#Default code#>
+    }
+}
+
+// Conditional extension
+extension ___FILEBASENAME___ where Self: <#UIView for example#> {
+    func <#functionName#>(<#parameter#>: <#Type#>) {
+        <#Specific code#>
+    }
+}
