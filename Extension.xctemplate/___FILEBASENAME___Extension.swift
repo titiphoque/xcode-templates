f8a7b3e8a9a748da1dbbf93f7496b8d378717487
Thibaut Schmitt <t.schmitt@openium.fr>
2022-11-14 11:49:25 +0100

    Add Extension template

---
+++ Extension.xctemplate/___FILEBASENAME___Extension.swift
@@ -0,0 +1,15 @@
+//
+// ___FILENAME___
+// ___PROJECTNAME___
+//
+// Created by ___FULLUSERNAME___ on ___DATE___.
+// ___COPYRIGHT___
+//
+
+import Foundation
+
+extension ___VARIABLE_productName___ {
+    func <#functionName#>(<#parameter#>: <#Type#>) {
+        <#code#>
+    }
+}
