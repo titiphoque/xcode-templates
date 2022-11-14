39d4a509049e2488320730b03849c902c8e9b230
Thibaut Schmitt <t.schmitt@openium.fr>
2022-11-14 11:57:47 +0100

    Add Singletion template

---
+++ Singleton.xctemplate/Default/___FILEBASENAME___.swift
@@ -0,0 +1,23 @@
+//
+// ___FILENAME___
+// ___PROJECTNAME___
+//
+// Created by ___FULLUSERNAME___ on ___DATE___.
+// ___COPYRIGHT___
+//
+
+import Foundation
+
+class ___FILEBASENAME___ {
+    static let shared = ___FILEBASENAME___()
+    
+    // MARK: - Init
+
+    private init() {
+        setup()
+    }
+    
+    private func setup() {
+        // Use this method to setup/configure your ___FILEBASENAME___
+    }
+}
---
+++ Singleton.xctemplate/NSObjectConformance/___FILEBASENAME___.swift
@@ -0,0 +1,24 @@
+//
+// ___FILENAME___
+// ___PROJECTNAME___
+//
+// Created by ___FULLUSERNAME___ on ___DATE___.
+// ___COPYRIGHT___
+//
+
+import Foundation
+
+class ___FILEBASENAME___: NSObject {
+    static let shared = ___FILEBASENAME___()
+    
+    // MARK: - Init
+
+    private override init() {
+        super.init()
+        setup()
+    }
+    
+    private func setup() {
+        // Use this method to setup/configure your ___FILEBASENAME___
+    }
+}
